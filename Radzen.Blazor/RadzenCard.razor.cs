7d8a0f39f5fd078832d9a2ab8452f533e15d8abe
Kasbolat Kumakhov <uhfath@gmail.com>
2024-04-30 16:02:31 +0300

    Добавлена возможность указания размера тени для компонента Card

--- Radzen.Blazor/RadzenCard.razor.cs
+++ Radzen.Blazor/RadzenCard.razor.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Radzen.Blazor.Rendering;
 
 namespace Radzen.Blazor
 {
@@ -11,14 +12,20 @@ namespace Radzen.Blazor
     /// </summary>
     public partial class RadzenCard : RadzenComponentWithChildren
     {
+        /// <summary>
+        /// Gets or sets the card shadow size.
+        /// </summary>
+        /// <value>The card shadow size.</value>
+        [Parameter]
+        public int? Shadow { get; set; }
+
         /// <inheritdoc />
         protected override string GetComponentCssClass()
         {
-            var classList = new List<string>();
-            classList.Add("rz-card");
-            classList.Add($"rz-variant-{Variant.ToString().ToLowerInvariant()}");
-
-            return string.Join(" ", classList);
+            return ClassList.Create("rz-card")
+                .Add($"rz-variant-{Variant.ToString().ToLowerInvariant()}")
+                .Add($"rz-shadow-{Shadow ?? 2}", Shadow.HasValue || Variant == Variant.Filled)
+                .ToString();
         }
 
         /// <summary>
